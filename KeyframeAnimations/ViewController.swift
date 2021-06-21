f582084b84363bb6965dabbf6cef224d22d1ba40
Duncan Champney <DChampney@wareto.com>
2021-06-21 07:46:26 -0400

    Initial Commit

---
+++ KeyframeAnimations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeyframeAnimations
+//
+//  Created by Duncan Champney on 6/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KeyframeAnimationsTests/KeyframeAnimationsTests.swift
@@ -0,0 +1,33 @@
+//
+//  KeyframeAnimationsTests.swift
+//  KeyframeAnimationsTests
+//
+//  Created by Duncan Champney on 6/21/21.
+//
+
+import XCTest
+@testable import KeyframeAnimations
+
+class KeyframeAnimationsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
